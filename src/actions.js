e2c3aa64669db7d0f3cb41170981ce06273239b0
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-07-15 21:26:45 -0400

    Added Redux-Thunk, Redux-Logger, Axios, Server.js, etc.

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,12 @@
+import Axios from 'axios';
+
 // action type
 export const MAKE_DEPOSIT = 'MAKE_DEPOSIT';
 export const MAKE_WITHDRAWAL = 'MAKE_WITHDRAWAL';
+
+export const GET_ACCOUNT_START = 'GET_ACCOUNT_START';
+export const GET_ACCOUNT_SUCCESS = 'GET_ACCOUNT_SUCCESS';
+export const GET_ACCOUNT_FAILED = 'GET_ACCOUNT_FAILED';
 
 // action creator
 export function makeDeposit(amount, account, description) {
@@ -23,5 +29,23 @@ export function makeWithdrawal(amount, account, description) {
       account,
       description
     }
+  };
+}
+
+export function getAccount() {
+  // can return a function because we're using redux-thunk
+  return dispatch => {
+    // receives direct access to the dispatcher
+    dispatch({ type: GET_ACCOUNT_START });
+
+    Axios.get('http://localhost:8080/')
+      .then(res => {
+        // console.log(res.data);
+        dispatch({ type: GET_ACCOUNT_SUCCESS, payload: res.data });
+      })
+      .catch(err => {
+        // console.log(err);
+        dispatch({ type: GET_ACCOUNT_FAILED, payload: err.response.data });
+      });
   };
 }
--- src/components/Withdrawal.js
+++ src/components/Withdrawal.js
@@ -1,96 +1,83 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { makeWithdrawal } from '../actions';
+import React from 'react'
+import { connect } from 'react-redux'
+import { makeWithdrawal } from '../actions'
 
 class Withdrawal extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      amount: '',
-      account: 'checking',
-      description: ''
-    };
-  }
+	constructor() {
+		super()
+		this.state = {
+			amount: '',
+			account: 'checking',
+			description: '',
+		}
+	}
 
-  handleChange = evt => {
-    evt.preventDefault();
+	handleChange = (evt) => {
+		evt.preventDefault()
 
-    this.setState({
-      [evt.target.name]: evt.target.value
-    });
-  };
+		this.setState({
+			[evt.target.name]: evt.target.value,
+		})
+	}
 
-  withdrawalMoney = evt => {
-    evt.preventDefault();
+	withdrawalMoney = (evt) => {
+		evt.preventDefault()
 
-    const { amount, account, description } = this.state;
-    // calling the action creator
-    this.props.makeWithdrawal(amount, account, description);
+		const { amount, account, description } = this.state
+		// calling the action creator
+		this.props.makeWithdrawal(amount, account, description)
 
-    // resetting the form after it submits
-    this.setState({
-      amount: '',
-      description: ''
-    });
-  };
+		// resetting the form after it submits
+		this.setState({
+			amount: '',
+			description: '',
+		})
+	}
 
-  render() {
-    const { total } = this.props;
-    const { amount, account, description } = this.state;
+	render() {
+		const { total } = this.props
+		const { amount, account, description } = this.state
 
-    return (
-      <section>
-        <h2>Make a Withdrawal</h2>
-        <h6>CURRENT TOTAL: ${total}</h6>
+		return (
+			<section>
+				<h2>Make a Withdrawal</h2>
+				<h6>CURRENT TOTAL: ${total}</h6>
 
-        <form onSubmit={this.withdrawalMoney}>
-          <input
-            type='number'
-            name='amount'
-            placeholder='Amount in USD'
-            value={amount}
-            onChange={this.handleChange}
-            required
-          />
+				<form onSubmit={this.withdrawalMoney}>
+					<input type="number" name="amount" placeholder="Amount in USD" value=
+					{amount} onChange={this.handleChange} required />
+					
+					<span> to </span>
+					
+					<select name="account" value={account} onChange={this.handleChange}>
+						<option value="checking">Checking</option>
+						<option value="savings">Savings</option>
+					</select>
+					
+					<br />
 
-          <span> to </span>
+					<input type="text" name="description" placeholder="Description" value={description} onChange={this.handleChange} required />
 
-          <select name='account' value={account} onChange={this.handleChange}>
-            <option value='checking'>Checking</option>
-            <option value='savings'>Savings</option>
-          </select>
+					<br />
 
-          <br />
-
-          <input
-            type='text'
-            name='description'
-            placeholder='Description'
-            value={description}
-            onChange={this.handleChange}
-            required
-          />
-
-          <br />
-
-          <button type='submit'>Withdrawal</button>
-        </form>
-      </section>
-    );
-  }
+					<button type="submit">Withdrawal</button>
+				</form>
+			</section>
+		)
+	}
 }
 
-const mapStateToProps = state => {
-  return {
-    total: state.checking + state.savings
-  };
-};
+const mapStateToProps = (state) => {
+	return {
+		total: state.checking + state.savings,
+	}
+}
 
 const mapDispatchToProps = {
-  makeWithdrawal: makeWithdrawal
-};
+	makeWithdrawal: makeWithdrawal,
+}
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Withdrawal);
+	mapStateToProps,
+	mapDispatchToProps,
+)(Withdrawal)
